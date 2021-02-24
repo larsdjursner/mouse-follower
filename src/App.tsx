5f9095f2c9f94d55ae44fe47f40067a343644cca
larsdjursner <40217101+larsdjursner@users.noreply.github.com>
2021-02-24 16:02:31 +0100

    dynamic resize of mouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-const circleRadius = 10;
-const initialMousePosition = { x:0, y:0 };
+// const width = window.innerWidth;
+// const height = window.innerHeight;
 
 const App = () => {
+  const circleRadius = 10;
+  const initialMousePosition = { x: 0, y: 0 };
   const [mousePos, setMousePos] = useState(initialMousePosition);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
-  const handleMouseMove = useCallback((event) => {
-    console.log(event.clientX);
-    const { clientX, clientY } = event;
-    setMousePos({ x: clientX, y: clientY });
-  },[setMousePos]);
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      console.log(event.clientX);
+      const { clientX, clientY } = event;
+      setMousePos({ x: clientX, y: clientY });
+    },
+    [setMousePos]
+  );
 
   return (
     <svg width={width} height={height} onMouseMove={handleMouseMove}>
