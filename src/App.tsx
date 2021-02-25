a3613dd442aa3e2388f9727cd25a213ac8a071eb
larsdjursner <40217101+larsdjursner@users.noreply.github.com>
2021-02-25 11:17:55 +0100

    added rect for future stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import React, { useCallback, useEffect, useState } from "react";
 // const height = window.innerHeight;
 
 const App = () => {
-  const circleRadius = 10;
-  const initialMousePosition = { x: 0, y: 0 };
-  const [mousePos, setMousePos] = useState(initialMousePosition);
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
+  const circleRadius = 10;
+  const initialMousePosition = { x: 0, y: 0 };
+  const initialRectPosition = { x: width/2, y: height/2 };
+  const initialRectDimensions = { width: 100, height: 100 };
+  
+  const [mousePos, setMousePos] = useState(initialMousePosition);
+  const [rectPos, setRectPos] = useState(initialRectPosition);
+  const [rectDim, setRectDim] = useState(initialRectDimensions);
+ 
 
   const updateDimensions = () => {
     setWidth(window.innerWidth);
@@ -17,21 +23,28 @@ const App = () => {
 
   useEffect(() => {
     window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions); //unmount
   }, []);
 
   const handleMouseMove = useCallback(
     (event) => {
-      console.log(event.clientX);
       const { clientX, clientY } = event;
       setMousePos({ x: clientX, y: clientY });
+
+
+      if((mousePos.x >= rectPos.x && mousePos.x <= rectPos.x +rectDim.width) && (mousePos.x >= rectPos.y && mousePos.y <= rectPos.y - rectDim.height)) {
+        console.log('hit')
+      }
+
+
     },
-    [setMousePos]
+    [mousePos, rectDim, rectPos]
   );
 
   return (
     <svg width={width} height={height} onMouseMove={handleMouseMove}>
       <circle cx={mousePos.x} cy={mousePos.y} r={circleRadius} />
+      <rect x={rectPos.x} y={rectPos.y} width={rectDim.width} height={rectDim.height}/>
     </svg>
   );
 };
